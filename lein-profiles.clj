a17e6ed9cb2993857a2e362e577233db195527d1
Hamish Rickerby <hamish@happtic.com>
2018-01-18 21:28:08 +1100

    Update dependencies for lein

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -1,9 +1,9 @@
 {
- :repl {:plugins [[cider/cider-nrepl "0.15.0"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.16.0"]]}
  :user {:dependencies [[pjstadig/humane-test-output "0.8.3"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
-        :plugins [[com.jakemccrary/lein-test-refresh "0.21.1"]
+        :plugins [[com.jakemccrary/lein-test-refresh "0.22.0"]
                   [jonase/eastwood "0.2.5"]
                   [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]]
         :signing {:gpg-key "843BBC8AF7927761DF290561E7FC3852D9E58138"}
