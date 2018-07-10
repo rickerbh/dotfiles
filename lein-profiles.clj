e8a142e206831a3fca8e437d0713bfd02ab14560
Hamish Rickerby <hamish@happtic.com>
2018-07-10 10:21:44 +1000

    Update eastwood version

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -4,7 +4,7 @@
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
         :plugins [[com.jakemccrary/lein-test-refresh "0.22.0"]
-                  [jonase/eastwood "0.2.5"]
+                  [jonase/eastwood "0.2.6"]
                   [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]]
         :signing {:gpg-key "843BBC8AF7927761DF290561E7FC3852D9E58138"}
         :test-refresh {:notify-command ["terminal-notifier" "-title" "Tests" "-message"]
