b7f8498b30268ffb5b7106f581c24fa797467f21
Hamish Rickerby <hamish@happtic.com>
2020-12-09 13:25:21 +1300

    Update lein-profiles.clj

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -1,12 +1,12 @@
 {
- :repl {:plugins [[cider/cider-nrepl "0.21.1"]]}
- :user {:dependencies [[pjstadig/humane-test-output "0.9.0"]]
+ :repl {:plugins [[cider/cider-nrepl "0.25.5"]]}
+ :user {:dependencies [[pjstadig/humane-test-output "0.10.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
         :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
-                  [jonase/eastwood "0.3.5"]
-                  [lein-cloverage "1.1.1"]
-                  [lein-kibit "0.1.6" :exclusions [org.clojure/clojure]]]
+                  [jonase/eastwood "0.3.12"]
+                  [lein-cloverage "1.2.1"]
+                  [lein-kibit "0.1.8" :exclusions [org.clojure/clojure]]]
         :signing {:gpg-key "843BBC8AF7927761DF290561E7FC3852D9E58138"}
         :test-refresh {:notify-command ["terminal-notifier" "-title" "Tests" "-message"]
                        :quiet true
